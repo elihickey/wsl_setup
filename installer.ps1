167f94855b9e53ed893cc16a368edfefb4c9ccfa
Eli Hickey <eli.hickey@ellucian.com>
2022-10-13 08:16:04 -0400

    installer added

---
+++ installer.ps1
@@ -0,0 +1,27 @@
+# 1. Make sure the Microsoft App Installer is installed:
+#    https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1
+# 2. Run this script as administrator.
+
+# Write-Output "Installing Apps: Git, VS Code, Terminal, and Ubunto if needed"
+# $apps = @(
+#     @{name = "Git.Git" },
+#     @{name = "Microsoft.VisualStudioCode" },
+#     @{name = "Microsoft.WindowsTerminal" },
+#     @{name = "Canonical.Ubuntu" })
+
+# Foreach ($app in $apps) {
+#     $listApp = winget list --exact -q $app.name
+#     if (![String]::Join("", $listApp).Contains($app.name)) {
+#         Write-host "Installing: " $app.name
+#         winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+#     }
+#     else {
+#         Write-host  $app.name " (already installed)"
+#     }
+# }
+
+Write-Output "Setting Up WSL"
+wsl --set-default-version 2
+$versions = wsl --list --verbose
+
+$x=1
